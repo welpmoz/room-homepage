c738fa12d4237888967a5906c6ca9aec2012da19
roswell <roswell_pando@outlook.es>
2025-05-09 18:11:47 -0500

    animating carousel using Transition component vue

--- src/components/carousel.vue
+++ src/components/carousel.vue
@@ -79,17 +79,39 @@ onUpdated(() => {
           <use xlink:href="#icon-angle-right" /></svg
       ></Button>
     </div>
-    <Panel
-      v-show="currentPanel === index"
-      v-for="(item, index) in items"
-      :key="item.title"
-      v-bind="item"
-    />
+    <Transition mode="out-in">
+      <Panel
+        v-if="currentPanel === 0"
+        :key="items[0].title"
+        v-bind="items[0]"
+      />
+      <Panel
+        v-else-if="currentPanel === 1"
+        :key="items[1].title"
+        v-bind="items[1]"
+      />
+      <Panel
+        v-else-if="currentPanel === 2"
+        :key="items[2].title"
+        v-bind="items[2]"
+      />
+    </Transition>
     <div class="u-visually-hidden" ref="alertsDiv"></div>
   </section>
 </template>
 <style scoped lang="scss">
 @use "../scss/functions" as f;
+
+.v-enter-active,
+.v-leave-active {
+  transition: all 0.5s ease;
+}
+
+.v-enter-from,
+.v-leave-to {
+  transform: translateX(-30px);
+  opacity: 0;
+}
 
 .c-carousel {
   position: relative;
@@ -98,6 +120,7 @@ onUpdated(() => {
     position: absolute;
     top: 30.4rem;
     right: 0rem;
+    z-index: 5;
 
     @include f.breakpoint(medium) {
       top: 37rem;
